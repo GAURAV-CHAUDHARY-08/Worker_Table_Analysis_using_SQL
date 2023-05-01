23b6869bf00eff8d21e81b1b964d5a845916631c
Gaurav Chaudhary <gc5317436@gmail.com>
2023-05-01 11:35:34 +0530

    Update Worker_Table_Analysis.sql

--- Worker_Table_Analysis_using_SQL/Worker_Table_Analysis.sql
+++ Worker_Table_Analysis_using_SQL/Worker_Table_Analysis.sql
@@ -98,3 +98,57 @@ SELECT WORKER_ID,FIRST_NAME,LAST_NAME,SALARY,JOINING_DATE,DEPARTMENT,WORKER_TITL
 FULL JOIN TITLE
 ON WORKER_ID = WORKER_REF_ID
 ORDER BY WORKER_ID;
+
+
+#Write a SQL query to display 2nd to 5th character as Middle_Name from First_Name column along with all other columns from Worker table and also have Department columnvalues more than 2 characters.
+select WORKER_ID, mid(First_name, 2,5) AS Middle_Name , LAST_NAME, SALARY, JOINING_DATE, 
+DEPARTMENT 
+from worker where length(department)>2 ;
+
+
+#Write a SQL query to display No. Of Employment Years for each Worker till today along with all other columns from Worker table.
+select *,date_format(from_days(datediff(now(),joining_date)),'%Y')+0 as emp_years from worker;
+
+
+#Write a SQL query to display maximum and minimum salary of Workers for each Worker_title along with all other columns from Worker table.
+select *, max(salary) over (partition by DEPARTMENT ) as max_salary , min(salary) over (partition by 
+DEPARTMENT ) as min_sal from 
+(select a.*,b.worker_title from worker as a left join title as b on a.worker_id = b.worker_ref_id) as c;
+
+
+#Write a SQL query to display all the data about worker promoted in shortest time to it’s current worker title and also for longest time take for promotion worker.
+select *, dense_rank() over (order by Promoted_Worker desc) as slow_prom , dense_rank() over 
+(order by promoted_worker) as fast_prom from
+(select *, datediff(joining_date, affected_from) as Promoted_Worker from
+(select a.*,b.WORKER_TITLE, AFFECTED_FROM from worker as a left join title as b on a.WORKER_ID 
+= b.WORKER_REF_ID) as c) as d;
+
+
+#Write a SQL query to display all the data about worker got bonus recently and also for worker got bonus lately.
+select *, dense_rank() over (order by BONUS_DATE desc) as recen_got_bon , dense_rank() over 
+(order by BONUS_DATE ) as latel_got_bon from
+(select a.*,b.BONUS_DATE,BONUS_AMOUNT from worker as a inner join bonus as b on 
+a.WORKER_ID = b.WORKER_REF_ID) as c;
+
+
+#Write a SQL query to display all records of Workers who received Bonus after the promotion and arrange them according to the bonus amount received from heighest to lowest.
+select a.*,b.BONUS_DATE,BONUS_AMOUNT from worker as a inner join bonus as b on 
+a.WORKER_ID = b.WORKER_REF_ID order by BONUS_AMOUNT desc;
+
+
+#Write a SQL query to display all records of Worker having 2nd Highest salary in their respective departments after adding their bonus amount into their respective salary and also display all records of worker having 2ndlowest salary in their respective departments after adding their bonus amount into their respective salary.
+select * from
+(select * , dense_rank() over(partition by department order by added_salary ) as Highest_earner,
+dense_rank() over(partition by department order by added_salary DESC ) as Lowest_earner from
+(select a.*,b.BONUS_AMOUNT,SALARY+ifnull(BONUS_AMOUNT,0) as added_salary from worker a 
+LEFT JOIN bonus b on a.WORKER_ID=b.WORKER_REF_ID ) as c) as d
+where (Highest_earner = 2 or Lowest_earner = 2 );
+
+
+#Write a SQL query to display all records of Worker fetched in above question where the 2nd highest and 2ndlowest salary in their respective departments are different Workers.
+select * from
+(select * , dense_rank() over(partition by department order by added_salary ) as Highest_earner,
+dense_rank() over(partition by department order by added_salary DESC ) as Lowest_earner from
+(select a.*,b.BONUS_AMOUNT,SALARY+ifnull(BONUS_AMOUNT,0) as added_salary from worker a 
+LEFT JOIN bonus b on a.WORKER_ID=b.WORKER_REF_ID ) as c) as d
+where (Highest_earner = 2 or Lowest_earner = 2 ) and Highest_earner <> Lowest_earner
